f2c6db8365117ab8431c3984f95ec62670382c17
elhammey <elhammey@gmail.com>
2021-10-27 10:57:55 +0330

    Added profiles viewset

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
-
+from django.conf import settings
 
 # Create your models here.
 
@@ -32,3 +32,42 @@ class UserProfileManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database model for users in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """Retrieve full name of user"""
+        return self.name
+
+    def get_short_name(self):
+        """Retrieve shot name of user"""
+        return self.name
+
+    def __str__(self):
+        """Return string representation of our user"""
+        return self.email
+
+
+class ProfileFeedItem(models.Model):
+    """Profile status update"""
+    user_profile = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return the model as a string"""
+        return self.status_text
